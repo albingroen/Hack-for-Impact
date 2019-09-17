46463bdce0a87a885882dd47b666a5e3339a0bf7
Albin Groen <albin.groen@gmail.com>
2019-09-17 16:17:52 +0200

    🎉 Start on stage 4

--- src/pages/stages/stage3.js
+++ src/pages/stages/stage3.js
@@ -1,11 +1,19 @@
 import React from "react";
-import data from ../
+import { Button } from "antd";
+import groceries from "../../data/groceries.json";
 
-const Stage3 = ({leftovers}) => (
+const Stage3 = ({ leftovers }) => (
   //   Stage 3 - Do you also have this ingredient?
+
   <div>
     <h1>Do you also have these items?</h1>
+    {groceries.map(g => (
+      <div>
+        <h3>{g.title}</h3>
+      </div>
+    ))}
+    <Button block>Submit</Button>
   </div>
 );
 
-export default Stage3kk;
+export default Stage3;
