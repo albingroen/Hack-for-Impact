50c065a1ab00c4904b446ba5dfe976fd1d82c5c3
Jarno Vuorenmaa <jarno.vuorenmaa@someturva.fi>
2019-09-17 16:27:32 +0200

    Initial version of selecting leftovers

--- src/pages/stages/stage2.js
+++ src/pages/stages/stage2.js
@@ -1,11 +1,44 @@
 import React from "react";
+import { Button } from 'antd';
 
-const Stage2 = ({}) => (
-  // Stage 2 - Depending on the previous answer what do you have left over?
+import SelectList from '../../components/selectList';
+import _ from 'lodash';
 
-  <div>
-    <h1>Stage 2</h1>
-  </div>
-);
+import recipes from '../../data/recipes';
+
+class Stage2 extends React.Component {
+  state = {
+    selectedItems: recipes[this.props.previousMeal].ingredients
+  }
+
+  selectItem(item) {
+    if (this.state.selectedItems.indexOf(item) === -1) {
+      this.setState({
+        selectedItems: [...this.state.selectedItems, item],
+      });
+    } else {
+      this.setState({
+        selectedItems: _.pull(this.state.selectedItems, item),
+      })
+    }
+    
+  }
+
+  render() {
+    const { onSubmit, previousMeal } = this.props;
+    const recipe = recipes[previousMeal];
+    const { title, ingredients } = recipe;
+    const {selectedItems} = this.state;
+    console.log(selectedItems);
+    return (
+      <div>
+        <h2>You've selected last time {title}</h2>
+        <div>Tell us what has left over</div>
+        <SelectList items={ingredients} selectedItems={selectedItems} onSelect={this.selectItem.bind(this)} />
+        <Button onClick={() => onSubmit(selectedItems)} type="primary">Confirm</Button>
+      </div>
+    );
+  }
+}
 
 export default Stage2;
